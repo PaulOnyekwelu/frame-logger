2d1fb18ac5aeae1e928e7a3e8e829f4bd67b5231
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-07-24 22:35:45 +0100

    validating of passed in arguments

--- frame-print/index.js
+++ frame-print/index.js
@@ -1,4 +1,9 @@
 module.exports = (message, pattern) => {
+    if(!message || !pattern){
+        throw new Error('frame-print requires two positional arguments!')
+    }
+    message = message.toString();
+    pattern = pattern.toString();
     console.log(pattern.repeat(message.length));
     console.log(message);
     console.log(pattern.repeat(message.length));
